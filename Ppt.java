a31bf86d10ad1cc1487997edc9fa476a18a7feff
Brena Santos <frescotomate123@gmail.com>
2025-07-27 21:58:08 -0300

    implementacao dos desenhos de cada alternativa e o pareamento da alternmativa com o desenho  ultilizando hashtable

--- Ppt.java
+++ Ppt.java
@@ -3,20 +3,54 @@ import java.util.*;
 public class Ppt {
 
     public static void main(String[] args) {
-
+        Hashtable<Integer, String> desenho = new Hashtable<>();
         Scanner leitor = new Scanner(System.in);
         Random gerador = new Random();
 
         int jogador;
         int maquina;
 
+        String pedra = ( """
+                _____
+            ---' ____)
+                (_____)
+                (_____)
+                (____)
+          ---.__(___)
+            """ );
+    
+        String papel = ( """
+                _____
+            ---' ____)____
+                    ______)
+                    _______)
+                    _______)
+            ---.__________)
+            """ );
+
+        String tesoura = ( """
+                _____
+            ---' ____)____
+                    ______)
+                __________)
+                (____)
+          ---.__(___)
+            """ );
+
+        // chaveando as alternativas com os desenhos
+        desenho.put(0, pedra);
+        desenho.put(1, papel);
+        desenho.put(2, tesoura);
+
+
         maquina = gerador.nextInt(3);
 
         // menu
-        System.out.println("0 - pedra");
-        System.out.println("1 - papel");
-        System.out.println("2 - tesoura");
+        System.out.println("0 - pedra" + desenho.get(0));
+        System.out.println("1 - papel"+ desenho.get(1));
+        System.out.println("2 - tesoura"+ desenho.get(2));
 
+        // validacao para nao passar outra alterantiva
         do {
             System.out.print("Jogador, insira sua jogada (0, 1 ou 2): ");
             jogador = leitor.nextInt();
@@ -24,6 +58,10 @@ public class Ppt {
                 System.out.println("Entrada inválida! Tente novamente.");
             }
         } while (jogador < 0 || jogador > 2);
+
+        // mostra escolhas dos jogadores
+        System.out.println("Sua jogada foi: " + jogador + desenho.get(jogador));
+        System.out.println("A jogada da maquina foi: " + maquina + desenho.get(maquina));
 
         // chama o método estático para verificar o resultado
         possibilidade(jogador, maquina);
@@ -45,4 +83,6 @@ public class Ppt {
         }
 
     }
+    
 }
+
